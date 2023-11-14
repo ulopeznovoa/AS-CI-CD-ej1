ac4248bd6ea62ff1cf60eed95c9cc744db147735
Aingeru <81016657+AingeruBeOr@users.noreply.github.com>
2023-11-14 15:43:02 +0100

    Update wallet.py: Bug fixed

--- wallet.py
+++ wallet.py
@@ -3,7 +3,7 @@ class Wallet:
         self.balance = balance
 
     def set_balance(self, val):
-        self.balance = self.balance + val + 20 # Bug here "+20"
+        self.balance = self.balance + val
 
     def get_balance(self):
         return self.balance
