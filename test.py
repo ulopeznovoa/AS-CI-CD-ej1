4dcb67d963dbb65234166da2c3ee6836966065a6
NagoreGomez <81180039+NagoreGomez@users.noreply.github.com>
2023-11-14 16:45:29 +0100

    Update test.py

--- test.py
+++ test.py
@@ -14,3 +14,8 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+
+def test_incrementar():
+    obj = Wallet(10)
+    obj.incrementar()
+    assert obj.get_balance() == 1010
