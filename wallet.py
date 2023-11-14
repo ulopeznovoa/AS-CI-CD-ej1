c0b56b81b8ccd55aa316cc0892070e2bd728864c
Galder Rodríguez <91203058+Zodiak811@users.noreply.github.com>
2023-11-14 15:41:30 +0100

    Update wallet.py: arreglado

--- wallet.py
+++ wallet.py
@@ -3,10 +3,10 @@ class Wallet:
         self.balance = balance
 
     def set_balance(self, val):
-        self.balance = self.balance + (val*2)
+        self.balance = self.balance + val
 
     def get_balance(self):
         return self.balance
 
     def remove_balance(self, val):
-        self.balance = self.balance + val
+        self.balance = self.balance - val
