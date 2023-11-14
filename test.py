9945e4e9719bdc8cb1eba4a72787b49efc31ef94
Patricia Ortega Garcia <patricia.ortega.garcia.02@gmail.com>
2023-11-14 16:44:42 +0100

    Update test.py: test loteria

--- test.py
+++ test.py
@@ -14,3 +14,9 @@ def test_setbalance():
     obj = Wallet(0)
     obj.set_balance(40)
     assert obj.get_balance() == 40
+
+def test_loteria()
+    obj = Wallet(0)
+    obj.set_balance(20)
+    obj.loteria()
+    assert obj.get_balance() == 1020
