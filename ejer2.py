8dd5f55475ec4377c57f290aaf79abd7a0a03dc9
Endikson <81328446+Endikson@users.noreply.github.com>
2024-01-15 10:10:28 +0100

    Create ejer2.py

---
+++ ejer2.py
@@ -0,0 +1,12 @@
+# main.py
+
+def sumar_numeros(num1, num2):
+    return num1 + num2
+
+# Dos variables con números
+numero1 = 5
+numero2 = 10
+
+# Mostrar la suma de los dos números
+resultado = sumar_numeros(numero1, numero2)
+print(f"La suma de {numero1} y {numero2} es: {resultado}")
